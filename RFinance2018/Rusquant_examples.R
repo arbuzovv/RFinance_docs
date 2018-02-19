51e6db0afce35e08480b6994b09348711f1d2385
arbuzovv <arbuzov1989@gmail.com>
2018-02-19 13:35:41 +0500

    fix changes

--- RFinance2018/Rusquant_examples.R
+++ RFinance2018/Rusquant_examples.R
@@ -10,7 +10,7 @@
 # GNU General Public License for more details.
 ###############################################################################
 # Examples for the R/Finance Presentation
-# Copyright (C) 2017  Vyacheslav Arbuzov
+# Copyright (C) 2018  Vyacheslav Arbuzov
 #
 # For more information please visit my site at www.rusquant.ru
 # or drop me a line at arbuzov1989@gmail.com
@@ -18,84 +18,72 @@
 
 
 #############################################################################
-# Dividend_strategy - towards to the efficient market
-# R/Finance 2017
+# Rusquant: another way trading with R
+# R/Finance 2018
 ###############################################################################
 
-#getDividends
+library(devtools)
+install_github('arbuzovv/rusquant') # the last version of package
 library(quantmod)
-library(PerformanceAnalytics)
-library(data.table)
-library(scales)
-library(xts)
+library(rusquant)
 
-stock_names <- read.csv('https://raw.githubusercontent.com/arbuzovv/RFinance_docs/master/RFinance2017/stock_list_1850.txt',stringsAsFactors = FALSE)
 
-#try to donwload dividends data, it needs few minutes for execution
-for(i in 1:length(stock_names[,1]))
+stock_names <- loadSymbolList(src = 'poloniex')
+
+#download OHLCV data
+price_data <- getSymbols(stock_names$Symbol[1],src='poloniex',auto.assign = FALSE)
+candleChart(price_data['2017-10/'])
+
+#load last trades
+trades <- getTradelog(Symbols = stock_names$Symbol[1],auto.assign = FALSE,from = Sys.Date()-1,to = Sys.time())
+buy_trades_imbalance <- sum(tail(trades,30)[,6])/length(tail(trades,30)[,6])
+
+#calc current order book imbalance
+order_book <- getOrderbook(Symbols = stock_names$Symbol[1],auto.assign = FALSE)
+buy_imbalance_ratio <- sum(order_book[order_book$isAsk==0,]$Volume)/sum(order_book$Volume)
+
+
+# simply momentum strategy 
+conn <- Connect() # connect to exchange
+
+position <- getPosition(conn,Symbol = stock_names$Symbol[1])
+open_orders <- getOpenOrders(conn,Symbol = stock_names$Symbol[1])
+
+if(position==0 & open_orders==NULL)
 {
-  x <- try(getDividends(stock_names[i,1],from = "1990-01-01"),silent = TRUE)
-  if(class(x)[1]!="logical" &  class(x)[1]!="try-error" & length(x)!=0)
-  {
-    dividens <- data.frame(stock_names[i,1],index(x),x)
-    names(dividens) <- c('Symbol','Date','Dividend')
-    if(exists('dividens_list') == TRUE)
-      dividens_list <- rbind(dividens_list,dividens)
-    if(exists('dividens_list') == FALSE)
-      dividens_list <- dividens
-  }
-  print(c(i,stock_names[i,1]))
-}
-View(dividens_list)
-
-#try to download price data, it needs few minutes for execution
-for(i in 1433:length(stock_names[,1]))
-{
-  x <- try(getSymbols(stock_names[i,1],from = "1990-01-01",auto.assign = FALSE),silent = TRUE)
-  if(class(x)[1]!="logical" &  class(x)[1]!="try-error" & length(x)!=0)
-  {
-    stock_price <- data.frame(stock_names[i,1],index(x),x)
-    names(stock_price) <- c('Symbol','Date','Open','High','Low','Close','Volume','Adjusted')
-    if(exists('stock_price_list') == TRUE)
-      stock_price_list <- rbind(stock_price_list,stock_price)
-    if(exists('stock_price_list') == FALSE)
-      stock_price_list <- stock_price
-  }
-  print(c(i,stock_names[i,1]))
+#open position when there is a signal
+	if(buy_trades_imbalance>0.7 & buy_imbalance_ratio>0.7)
+	{
+	lmt_price <- max(order_book[order_book$isAsk==0,1]) # set price as best bid
+	if(lmt_price >= range(trades[,3])[1] & lmt_price <= range(trades[,3])[2]) # if price is correct
+		{
+		lmt_order <- Order(Symbol = stock_names$Symbol[1], action = "BUY", Quantity = 1, orderType = "LMT", Price = lmt_price)
+		ord_id <- openOrder(conn, lmt_order)
+		print(ord_id)
+		}
+	}
+#open position when there is a signal
+	if(buy_trades_imbalance<0.3 & buy_imbalance_ratio<0.3)
+	{
+	lmt_price <- min(order_book[order_book$isAsk==1,1]) # set price as best bid
+	if(lmt_price >= range(trades[,3])[1] & lmt_price <= range(trades[,3])[2]) # if price is correct
+		{
+		lmt_order <- Order(Symbol = stock_names$Symbol[1], action = "SELL", Quantity = 1, orderType = "LMT", Price = lmt_price)
+		ord_id <- openOrder(conn, lmt_order)
+		print(ord_id)
+		}
+	}
 }
 
-#Figure 1. Distribution of dividend yield
-rel_div <- merge(dividens_list,stock_price_list,by = c('Symbol','Date'),all.x = TRUE)
-rel_div$dividend_yield <- 100*rel_div$Dividend/rel_div$Open
-hist(rel_div$dividend_yield,breaks = 400000,xlim=c(0,4),xlab='Dividend yield',main='')
+#close position without signal
+if(position>0)
+	if(buy_trades_imbalance < 0.7 | buy_imbalance_ratio < 0.7)
+		lmt_order <- Order(Symbol = stock_names$Symbol[1], action = "SELL", Quantity = position[1], orderType = "MKT", Price = 0)
+#close position without signal
+if(position<0)
+	if(buy_trades_imbalance > 0.3 | buy_imbalance_ratio > 0.3)
+		lmt_order <- Order(Symbol = stock_names$Symbol[1], action = "BUY", Quantity = position[1], orderType = "MKT", Price = 0)
 
-# Figure 2. Scatterplot of dividend yield and night ex-dividend return
-stock_price_list <- data.table(stock_price_list)
-night_gaps <- data.frame(stock_price_list[,list(Date[2:length(Date)],NightGap = Open[2:(length(Open))]/Close[1:length(Close)]-1),by='Symbol'])
-names(night_gaps)[2] <- 'Date'
-div_returns <- merge(rel_div,night_gaps,by = c('Symbol','Date'),all.x = TRUE)
-plot(div_returns$dividend_yield,100*div_returns$NightGap,xlim = c(0,4),ylim=c(-3,3),ylab = 'Night gap return, %',xlab = 'Dividend yield, %',cex=.1,col=alpha('darkgreen',0.2))
-grid()
 
-#clean data
-performance <- div_returns[div_returns$dividend_yield < 10,]
-performance <- performance[performance$dividend_yield > 0,]
-performance <- performance[performance$NightGap < 0.5,]
-performance <- performance[performance$NightGap > -0.5,]
-performance <- performance[performance$Date > as.Date('2012-01-01'),]
-performance$day_pnl <- performance$dividend_yield/100+performance$NightGap
 
-#pnl distribution
-hist(performance$day_pnl,breaks = 1000,xlim=c(-0.07,0.07))
-
-#calc performance
-strategy_pnl <- aggregate(performance$day_pnl,by=list(performance$Date),mean)
-names(strategy_pnl) <-  c('Date','PnL')
-strategy_pnl <- xts(strategy_pnl$PnL,order.by = strategy_pnl$Date)
-
-#commission
-strategy_pnl <- strategy_pnl - 6/10000
-par(mfrow=c(2,1))
-chart.CumReturns(strategy_pnl ,main=SharpeRatio.annualized(strategy_pnl),geometric = F,wealth.index = F)
-chart.Drawdown(strategy_pnl,main="Drawdown from Peak Equity Attained",geometric = F)
 
